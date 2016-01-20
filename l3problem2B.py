20eb3a346b3c7a91992c7c9ababa1f909ff9737a
Juan Roa <jdroa92@gmail.com>
2016-01-19 22:39:10 -0500

    new tests for unit 2

---
+++ l3problem2B.py
@@ -0,0 +1,5 @@
+a = 10
+print("Hello!")
+while a >= 2:
+    print(a)
+    a -= 2
---
+++ l3problem2C.py
@@ -0,0 +1,18 @@
+# REVIEW THIS CODE
+
+end = raw_input("Enter and integer value: ")
+i = 1
+j = 0
+int(end)
+while i <= end:
+    j += i
+    i += 1
+print(j)
+
+# For edx's problem
+# i = 1
+# j = 0
+# while i <= end:
+#     j += i
+#     i += 1
+# print(j)
